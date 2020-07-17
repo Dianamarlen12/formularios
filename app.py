6c661077e26d8a077be7a1590fb41f62e85e9d5c
Dianamarlen12 <50559771+Dianamarlen12@users.noreply.github.com>
2020-07-16 19:12:59 -0500

    Correcto

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 import web
 
 urls = (
-    '/delete', 'mvc.controllers.personas.delete.Delete',
-    '/insert', 'mvc.controllers.personas.index.Index',
-    '/', 'mvc.controllers.personas.insert.Insert',
-    '/list', 'mvc.controllers.personas.list.List',
-    '/update', 'mvc.controllers.personas.update.Update',
-    '/view', 'mvc.controllers.personas.view.View',
+    '/', 'mvc.controllers.index.Index',
+
+    '/delete', 'mvc.controllers.alumnos.delete.Delete',
+    '/insert', 'mvc.controllers.alumnos.insert.Insert',
+    '/list', 'mvc.controllers.alumnos.list.List',
+    '/update', 'mvc.controllers.alumnos.update.Update',
+    '/view', 'mvc.controllers.alumnos.view.View',
 )
 app = web.application(urls, globals())
 
+
 if __name__ == "__main__":
-    web.httpserver.runsimple(app.wsgifunc(), ("0.0.0.0", 8090))
     app.run()
